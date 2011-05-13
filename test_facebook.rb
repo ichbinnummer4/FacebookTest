c0cf9167c7f1fdc81df14664afa46114336fc950
Nummer Vier <ichbinnummer4@googlemail.com>
2011-05-13 11:35:44 +0200

    Refactored login class and something else

--- test_facebook.rb
+++ test_facebook.rb
@@ -11,60 +11,65 @@ yml = YAML::load(File.open('ondemand.yml'))
 Sauce.config do |config|
   config.browser_url = "http://www.facebook.com/"
   config.browsers = [
-	["Linux", "firefox", "3.6."],
-	["Windows 2003", "iexplore", "8"]
-  ]
+	["Linux", "firefox", "3.6."]#,
+	# ["Windows 2003", "iexplore", "8"],
+	# ["Windows 2008", "iexplore", "9."],
+	# ["Windows 2003", "googlechrome", ""], # since Chrome is frequently and automatically updated, leave version empty
+	# ["Windows 2003", "safari", "4."]
+  ]# if it's not necessary to test all browsers, then this part can be set as a block receiver by request
   config.username = yml[:username]
   config.access_key = yml[:api_key]
 end
 
+class LoginPage
+	def initialize(driver)
+		@driver = driver
+	end
+	def login_as(user)
+		@driver.instance_eval do
+			open("/") # open the base URL which set as browser_url
+			type("id=email", user[:email])
+			type("id=pass", user[:pass])
+			click("xpath=//input[@type='submit' and @value='Login']")
+			wait_for_page_to_load("40000")
+		end
+	end
+end
+
 class FacebookTest < Sauce::TestCase
+
 	def test_login
-		page.open("/")
-		page.type("id=email", "ichbinnummer4@googlemail.com")
-		page.type("id=pass", "number4rocks")
-		page.click("xpath=//input[@type='submit' and @value='Login']")
-		page.wait_for_page_to_load("30000")
-		assert page.is_text_present("Account")
-		# page.verifyText("navAccountLink", "Account")
-		# page.verifyTextPresent("Account")
-		# begin
-			# assert page.is_text_present("asdfkla")
-		# rescue Test::Unit::AssertionFailedError
-			# @verification_errors << $!
-		# end
-    end
+		puts "\nTest Login is executing..."
+		LoginPage.new(page).login_as(:email => "ichbinnummer4@googlemail.com", :pass => "number4rocks")
+		assert_equal("Account", page.get_text("id=navAccountLink"))
+	end
 
-	# ensure that it stays on Home page
-	#~ page.click("xpath=//h1[@id='pageLogo']/a[@title='Home']")
-
-    def test_change_status
-		page.open("/")
-		page.type("id=email", "ichbinnummer4@googlemail.com")
-		page.type("id=pass", "number4rocks")
-		page.click("xpath=//input[@type='submit' and @value='Login']")
-		page.wait_for_page_to_load("30000")
-		assert page.is_text_present("Account")
+	def test_change_status
+		puts "\nTest Change Status is executing..."
+		LoginPage.new(page).login_as(:email => "ichbinnummer4@googlemail.com", :pass => "number4rocks")
+		# make sure that it stays on Home page
 		page.click("xpath=//h1[@id='pageLogo']/a[@title='Home']")
 		regex_time = /^(.*?)\s(.*?)\s.*/
-		time_msg = "Current Date is: #{regex_time.match(Time.now.to_s)[1]}, and Time is: #{regex_time.match(Time.now.to_s)[2]}"
-		# page.type "xhpc_message_text", "#{time_msg}"
+		now = Time.now.to_s
+		time_msg = "Current Date is: #{regex_time.match(now)[1]}, and Time is: #{regex_time.match(now)[2]}"
 		page.type("xpath=//div[@class='uiTypeahead composerTypeahead mentionsTypeahead']/div[1]/div/textarea", "#{time_msg}")
 		page.click("xpath=//div[@data-name='UIPrivacyWidget[0]']//a[@role='button']")
 		page.click("xpath=//div[@data-name='UIPrivacyWidget[0]']/div/div/ul/li[2]/a/span")
 		page.click("xpath=//input[@type='submit' and @value='Share']")
-    end
+		# the latest posted status is always in the xpath of /li (since it's on top), otherwise there is an index after, e.g. /li[3]
+		msg = page.get_text("xpath=//li/div[@class='storyContent']//span[@class='messageBody']")
+		privacy = page.get_text("xpath=//li/div[@class='storyContent']//a[@class='uiTooltip uiStreamPrivacy']/span/span[@class='uiTooltipText uiTooltipNoWrap']")
+		assert_equal(time_msg, msg)
+		assert_equal("Friends of Friends", privacy)
+	end
 
-    def test_logout
-		page.open("/")
-		page.type("id=email", "ichbinnummer4@googlemail.com")
-		page.type("id=pass", "number4rocks")
-		page.click("xpath=//input[@type='submit' and @value='Login']")
-		page.wait_for_page_to_load("30000")
-		assert page.is_text_present("Account")
+	def test_logout
+		puts "\nTest Logout is executing..."
+		LoginPage.new(page).login_as(:email => "ichbinnummer4@googlemail.com", :pass => "number4rocks")
 		page.click("navAccountLink")
 		page.click("xpath=//input[@type='submit' and @value='Logout']")
-		page.wait_for_page_to_load "30000"
-		sleep 15
-    end
+		page.wait_for_page_to_load("40000")
+		assert page.is_element_present("xpath=//input[@type='submit' and @value='Login']")
+	end
+
 end
